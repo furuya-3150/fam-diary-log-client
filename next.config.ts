bbf63999d4abe4725b43daad90823e4fbd2ca050
taku <takumi.f.0308@icloud.com>
2026-03-11 09:04:25 +0900

    本番環境のログを削除

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: isProd
+      ? {
+          exclude: ["error"],
+        }
+      : false,
+  },
 };
 
 export default nextConfig;
